965023f5435cadb53e034fd67b8f91ca9483f20d
romanedorrel <romanedorrel@gmail.com>
2024-03-02 16:24:29 -0500

    module 5 #2

--- myapp/lab1.js
+++ myapp/lab1.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const app = express();
 const port = 2000;
-const port1 = 2001;
+const port2 = 2001;
+const port3 = 2500;
 
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
     res.send('Hello World!')
 })
 
-app.use('/', express.static('public'));
+app.use('/', express.static('public'))
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
 
-app.listen(port1, () => {
-    console.log(`Example app listening at http://localhost:${port1}`)
+app.listen(port2, () => {
+    console.log(`Example app listening at http://localhost:${port2}`)
+})
+
+app.listen(port3, () => {
+    console.log(`Example app listening at http://localhost:${port3}`)
 })
--- myapp/routes/calculatorRoutes.js
+++ myapp/routes/calculatorRoutes.js
@@ -2,18 +2,47 @@ const express = require('express');
 const calculatorController = require('../controllers/calculatorController')
 const router = express.Router();
 
-router.get('/add', (req, res) => {
-    calculatorController.addNumbers(req,res)
-    })
-module.exports = router;
-    // functional add route performing addition on request parameters
 // router.get('/add', (req, res) => {
-// let number1 = parseInt(req.query.num1);
-// let number2 = parseInt(req.query.num2);
-// let sum = number1 + number2
-// console.log(sum)
-// res.status(200)
-// res.json({result:sum})
-// })
-
+//     console.log(req.query);
+//     res.send(req.query);
+//     //calculatorController.addNumbers(req,res)
+//     })
 // module.exports = router;
+    // functional add route performing addition on request parameters
+router.get('/add', (req, res) => {
+    let number1 = parseInt(req.query.num1);
+    let number2 = parseInt(req.query.num2);
+    let sum = number1 + number2;
+    console.log(sum);
+    res.status(200);
+    res.json({result:sum});
+})
+
+router.get('/subtract', (req, res) => {
+    let number1 = parseInt(req.query.num1);
+    let number2 = parseInt(req.query.num2);
+    let difference = number1 - number2;
+    console.log(difference);
+    res.status(200);
+    res.json({result:difference});
+})
+
+router.get('/multiply', (req, res) => {
+    let number1 = parseInt(req.query.num1);
+    let number2 = parseInt(req.query.num2);
+    let product = number1 * number2;
+    console.log(product);
+    res.status(200);
+    res.json({result:product});
+})
+
+router.get('/divide', (req, res) => {
+    let number1 = parseInt(req.query.num1);
+    let number2 = parseInt(req.query.num2);
+    let quotient = number1 / number2;
+    console.log(quotient);
+    res.status(200);
+    res.json({result:quotient});
+})
+
+ module.exports = router;
