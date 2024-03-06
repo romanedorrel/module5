f128d5722325877bbdc8a4f052edeb7cea18bcbb
romanedorrel <romanedorrel@gmail.com>
2024-03-05 21:41:09 -0500

    updates to module 5

--- myapp/lab1.js
+++ myapp/lab1.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const app = express();
-const port = 2000;
-const port2 = 2001;
-const port3 = 2500;
+// const express = require('express');
+// const app = express();
+// const port = 2000;
+// const port2 = 2001;
+// const port3 = 2500;
 
-app.get('/test', (req, res) => {
-    res.send('Hello World!')
-})
+// app.get('/test', (req, res) => {
+//     res.send('Hello World!')
+// })
 
-app.use('/', express.static('public'))
+// app.use('/', express.static('public'))
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+// app.listen(port, () => {
+//     console.log(`Example app listening at http://localhost:${port}`)
+// })
 
-app.listen(port2, () => {
-    console.log(`Example app listening at http://localhost:${port2}`)
-})
+// app.listen(port2, () => {
+//     console.log(`Example app listening at http://localhost:${port2}`)
+// })
 
-app.listen(port3, () => {
-    console.log(`Example app listening at http://localhost:${port3}`)
-})
+// app.listen(port3, () => {
+//     console.log(`Example app listening at http://localhost:${port3}`)
+// })
